c642de67b9f1f6b4cfafb4257767bdeeae0569cf
maks88sgt <maks88sgt@yandex.ru>
2021-02-01 00:02:10 +0300

    Some code cleaning

--- lesson 5/server.js
+++ lesson 5/server.js
@@ -3,7 +3,7 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 const LocalStrategy = require('passport-local');
 const BearerStrategy = require('passport-http-bearer');
-const bodyParser = require ('body-parser');
+const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 
 const secretConfig = {
@@ -14,24 +14,18 @@ const app = express();
 
 const port = 8080;
 
-mongoose.connect('mongodb://localhost:27017', /*{ useNewUrlParser: true }*/);
+mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true ,  useUnifiedTopology: true });
 const UserSchema = mongoose.Schema({ name: String, password: String, jwt: String });
 const User = mongoose.model('Users', UserSchema);
 
-async function readUsersFromDatabase(database) {
-  return await database.find({});
-}
-
 const localStrategy = new LocalStrategy(
-  { usernameField: 'username', passwordField: 'password'},
+  { usernameField: 'username', passwordField: 'password' },
   (username, password, done) => {
-    console.log(username + ' ' + password);
-    User.find({ name: username, password: password }).exec().then ((foundedUsers) => {
-      console.log(foundedUsers);
+    User.find({ name: username, password: password }).exec().then((foundedUsers) => {
       if (!foundedUsers || !foundedUsers.length) {
-        done('Not found');
-      } else {
-        console.log(username + ' try to pass into');
+        done('Invalid username or password');
+      }
+      else {
         done(null, foundedUsers[0]);
       }
     });
@@ -39,34 +33,35 @@ const localStrategy = new LocalStrategy(
 );
 
 const bearerStrategy = new BearerStrategy((token, done) => {
-    User.findOne({ jwt: token }).exec().then ((foundedUser) => {
-      if (!foundedUser) {
-        done('Invalid token');
-      } else {
-        done(null, foundedUser);
-      }
-    });
+  User.findOne({ jwt: token }).exec().then((foundedUser) => {
+    if (!foundedUser) {
+      done('Invalid token');
+    }
+    else {
+      done(null, foundedUser);
+    }
+  });
 });
-
-app.use(bodyParser.json());
 
 passport.use('local', localStrategy);
 passport.use('bearer', bearerStrategy);
 
 passport.serializeUser((user, done) => {
   let token = jwt.sign(
-    {username: user.name},
+    { username: user.name },
     secretConfig.secret,
-    { expiresIn: '24h'},
+    { expiresIn: '24h' },
   );
   User.updateOne({ name: user.name }, { jwt: token }, (err, updatedUser) => {
-    done (null, updatedUser);
+    done(null, updatedUser);
   });
 });
 
 passport.deserializeUser((user, done) => {
-  done (null, user);
+  done(null, user);
 });
+
+app.use(bodyParser.json());
 
 app.use(passport.initialize());
 
@@ -81,12 +76,14 @@ app.get('/name', passport.authenticate('bearer', { session: false }), (request, 
 });
 
 
-
-app.listen(port, async (error) => {
+app.listen(port, (error) => {
   if (error) {
     return console.log('The exception is happened: ', error);
   }
   console.log(`Server is listening on ${port}`);
-  let usersFromDatabase = await readUsersFromDatabase(User);
-  console.log(`In the database now ${usersFromDatabase.length} users:\n`, usersFromDatabase.map((user) => user.name + '\t\t' + user.password + '\t\t' + user.jwt).join('\n'));
+  User.find({})
+    .exec()
+    .then((usersFromDatabase) => {
+      console.log(`In the database now ${usersFromDatabase.length} users:\n`, usersFromDatabase.map((user) => user.name + '\t\t' + user.password + '\t\t' + user.jwt).join('\n'))
+    });
 });
